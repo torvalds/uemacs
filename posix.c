1d0cfd02760c39c0c1ae0790d69391f29e19271e
Naohiro Aota <naota@gentoo.org>
2012-05-26 16:06:28 +0200

    Make uemacs build on FreeBSD.
    See <https://bugs.gentoo.org/show_bug.cgi?id=387135>.
    
    Signed-off-by: Ulrich Müller <ulm@gentoo.org>
    Signed-off-by: Linus Torvalds <torvalds@linux-foundation.org>

--- posix.c
+++ posix.c
@@ -25,7 +25,7 @@
 
 /* Since Mac OS X's termios.h doesn't have the following 2 macros, define them.
  */
-#if defined(SYSV) && defined(_DARWIN_C_SOURCE)
+#if defined(SYSV) && (defined(_DARWIN_C_SOURCE) || defined(_FREEBSD_C_SOURCE))
 #define OLCUC 0000002
 #define XCASE 0000004
 #endif
