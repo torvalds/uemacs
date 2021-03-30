1c1b25ef723c952ca557cb5ff6d8db159ef1d4bc
Linus Torvalds <torvalds@linux-foundation.org>
2021-03-30 09:53:34 -0700

    increase size of lock table
    When editing lots of files, the lock table size can become a limit.
    
    Increase it to be HUGE.
    
    Nobody should *ever* use this silly editor in this day and age any more.
    I just can't seem to unlearn my finger memory, and it's easier for me to
    do these stupid small fixes than it is to figure out how to do key
    remapping in some saner editor.
    
    Signed-off-by: Linus Torvalds <torvalds@linux-foundation.org>

--- estruct.h
+++ estruct.h
@@ -233,7 +233,7 @@
 #define NKBDM   256		/* # of strokes, keyboard macro */
 #define NPAT    128		/* # of bytes, pattern          */
 #define HUGE    1000		/* Huge number                  */
-#define	NLOCKS	100		/* max # of file locks active   */
+#define	NLOCKS	1000		/* max # of file locks active   */
 #define	NCOLORS	8		/* number of supported colors   */
 #define	KBLOCK	250		/* sizeof kill buffer chunks    */
 
