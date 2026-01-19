7f1a6a45ead8f20af6085ab154a0874078cd028b
Linus Torvalds <torvalds@linux-foundation.org>
2026-01-19 12:20:07 -0800

    Add local dictionaries if they exist
    This is very simplistic, but it adds a local dictionary file from the
    current working directory and from the home directory.
    
    The file naming matches what I did for HunspellColorize, but might not
    be TheRightThing(tm), and maybe there's some standard name for local
    dictionary files.
    
    Signed-off-by: Linus Torvalds <torvalds@linux-foundation.org>

--- main.c
+++ main.c
@@ -52,6 +52,7 @@
  */
 
 #include <stdio.h>
+#include <sys/stat.h>
 #include <hunspell.h>
 
 /* Make global definitions not external. */
@@ -89,6 +90,13 @@ int spellcheck(const char *word)
 	return Hunspell_spell(hunhandle, word);
 }
 
+static void local_dictionary(Hunhandle *handle, const char *filename)
+{
+	struct stat st;
+	if (!stat(filename, &st) && S_ISREG(st.st_mode))
+		Hunspell_add_dic(handle, filename);
+}
+
 int main(int argc, char **argv)
 {
 	int c = -1;	/* command character */
@@ -113,6 +121,15 @@ int main(int argc, char **argv)
 	const char *aff_path = "/usr/share/hunspell/en_US.aff";
 	const char *dic_path = "/usr/share/hunspell/en_US.dic";
 	hunhandle = Hunspell_create(aff_path, dic_path);
+	if (hunhandle) {
+		local_dictionary(hunhandle, ".dictionary");
+		const char *home = getenv("HOME");
+		if (home) {
+			char buf[1024];
+			snprintf(buf, sizeof(buf), "%s/.dictionary", home);
+			local_dictionary(hunhandle, buf);
+		}
+	}
 
 	signal(SIGWINCH, sizesignal);
 	if (argc == 2) {
